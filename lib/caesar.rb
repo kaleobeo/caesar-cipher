4656aefa870df03eaca626368cf4ce4b3d2a5964
Kaleo K <kaleo.kronberg@gmail.com>
2022-05-22 15:24:31 -0700

    Add first few tests

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,26 +1,23 @@
 
-def caesar_convert(string, shift_factor)
-  new_string = string
-  .split("")
-  .map do |char|
-    if char == " " || char == "." || char == "," || char == "!" || char == "'" || char == "\""
-      next char
-    end
-
-    if char.ord.between?(65, 90)
-      char_shifted = (65 + (((char.ord + shift_factor) - 65) % 26))
-    end
-    
-    if char.ord.between?(97, 122)
-      char_shifted = (97 + (((char.ord + shift_factor) - 97) % 26))
-    end
-
-    char_shifted.chr
-  end
-  .join
-end
-
-
-
-
+class Caesar
+  def convert(string, shift_factor)
+    new_string = string
+    .split('')
+    .map do |char|
+      if char == " " || char == "." || char == "," || char == "!" || char == "'" || char == "\""
+        next char
+      end
   
+      if char.ord.between?(65, 90)
+        char_shifted = (65 + (((char.ord + shift_factor) - 65) % 26))
+      end
+  
+      if char.ord.between?(97, 122)
+        char_shifted = (97 + (((char.ord + shift_factor) - 97) % 26))
+      end
+  
+      char_shifted.chr
+    end
+    .join
+  end
+end
---
+++ spec/caesar_convert_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# spec/caesar_convert_spec.rb
+require './lib/caesar'
+
+describe Caesar do
+  describe '#convert' do
+    it 'works with a small positive shift' do
+      caesar = described_class.new
+      expect(caesar.convert('abc', 2)).to eql('cde')
+    end
+
+    it 'works with mixed cases' do
+      caesar = described_class.new
+      expect(caesar.convert('HeLLo', 5)).to eql('MjQQt')
+    end
+
+    it 'works with spaces and special characters' do
+      caesar = described_class.new
+      expect(caesar.convert('Hello, world!', 2)).to eql('Jgnnq, yqtnf!')
+    end
+  end
+end
